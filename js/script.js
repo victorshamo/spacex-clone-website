e6db705a29fa5d8761495b33f32ec243e46f4448
vh <37343169+victorshamo@users.noreply.github.com>
2024-02-24 19:08:06 +0000

    Feat: Dark Overlay

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
-const btn = document.getElementById('menu-btn');
+const btn = document.getElementById("menu-btn");
+const overlay = document.getElementById("overlay");
 
-btn.addEventListener('click', navToggle);
+btn.addEventListener("click", navToggle);
 
 function navToggle() {
-    btn.classList.toggle('open');
+  btn.classList.toggle("open");
+  overlay.classList.toggle("overlay-show");
 }
